bf410c45d5a0a7760186ccbec0646a578fe07fbd
turavey <turavey@gmail.com>
2020-01-08 10:27:45 +0200

    test

---
+++ addressbook_web.3/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace addressbook_web._3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
